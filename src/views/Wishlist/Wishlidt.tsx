4a6a215de59a843d848ea26a1e0830f1187abd95
Vitalii <vitaliycall@gmail.com>
2023-10-19 15:55:30 +0200

    )

--- src/views/Wishlist/Wishlidt.tsx
+++ src/views/Wishlist/Wishlidt.tsx
@@ -4,6 +4,7 @@ import { useDate } from '../../hook/useDate'
 import { useActions } from '../../hook/useActions'
 import Footer from '../../components/layout/footer/Footer'
 import Header from '../../components/layout/header/Header'
+import CartProduct from '../../components/CartProduct/CartProduct'
 
 const Wishlist = () => {
   const { wishlist } = useDate()
@@ -15,20 +16,26 @@ const Wishlist = () => {
   return(
     <>
     <Header />
-      <p>Wishlist</p>
-      <div onClick={()=>{delAllWish()}}>Удалить все</div>
-        {wishlist.wishlist.length?
-          wishlist.wishlist.map(e=>{
-            return(
-              <div key={e.id}>
-                <p>{e.product.name}</p>
-                <div onClick={()=>{delWish(e.id)}}>Удалить Товар</div>
-              </div>
-            )
-          })
-        : 
-        <p>There is no desire</p>
-        }
+      <div className={s.size}>
+        <div className={s.top}>
+          <div className={s.globalName}>Wishlist</div>
+          <div className={s.delButton} onClick={()=>{delAllWish()}}>Удалить все</div>
+        </div>
+          <div className={s.wishList}>
+            {wishlist.wishlist.length?
+              wishlist.wishlist.map(e=>{
+                return(
+                  <div className={s.post} key={e.id}>
+                    <CartProduct content={e.product} />
+                    <div className={s.del} onClick={()=>{delWish(e.id)}}>Удалить Товар</div>
+                  </div>
+                )
+              })
+            : 
+            <p>There is no desire</p>
+            }
+          </div>
+        </div>
     <Footer />
     </>
   )
